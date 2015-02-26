4a4ade606fb1a83d394940efd187553fac5e0fd7
Dj Gilcrease <dj.gilcrease@kanbansolutions.com>
2015-02-25 21:09:41 -0800

    Try to get the coverage report to post properly to coveralls

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
+import os
+import io
+from setuptools import setup, find_packages
 
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(cwd, 'README.md'), encoding='utf-8') as fd:
+    long_description = fd.read()
+
+
+setup(
+    name='repo-manager',
+    version='0.0.1',
+    description=('General purpose package repository manager that '
+                 'supports plugable metadata generators, and storage backends'),
+    long_description=long_description,
+    url='https://github.com/Digitalxero/repo-manager',
+    author='Dj Gilcrease',
+    author_email='digitalxero@gmail.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords=('createrepo'),
+    packages=find_packages(),
+)
